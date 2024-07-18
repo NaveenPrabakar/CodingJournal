2f2b559ff2da875ded6b9e22dfe4790541ac401a
NaveenPrabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-07-18 17:35:13 -0500

    Update PreviousJournal.py

--- PreviousJournal.py
+++ PreviousJournal.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect( #connects to MySQL Server for data to be accessed
     host = "localhost",
     user = "root",
-    password = "xxxxxxxx"
+    password = "defg5678"
 )
 
 mycursor = db.cursor()
@@ -38,12 +38,60 @@ def extract(date, name):
         print(f"Error: {err}")
 
 
+#Outputs the query results
 def query(results):
     for row in results:
         description = row[0]
         errors = row[1]
         buddy = row[2]
         todo = row[3]
-        print(f"Description: {description}\n Errors: {errors}\n Buddy: {buddy}\n Todo: {todo}\n")
+        additional = row[4]
+        print(f"Description: {description}\n Errors: {errors}\n Buddy: {buddy}\n Todo: {todo}\n additional: {additional}")
+
+
+
+def modify(date,name):
+
+    while True:
+        extract(date,name)
+        
+        choice = input("These were the entries found, would you like to edit the [d]escription, [e]rrors, [p]artners, [t]odo, [da]te, [o]thers or [q]uit ")
+
+        if choice == 'q':
+            break
+
+        edit = input("Enter the new change")
+        update(choice, edit, date)
+
+
+
+#updates the entries
+def update(choice, edit, date):
+    word = ""
+
+    match choice:
+        case "d":
+            word = "description"
+        case "e":
+            word = "errors"
+        case "p":
+            word = "buddy"
+        case "t":
+            word = "Todo"
+        case "o":
+            word = "additional"
+        case "da":
+            word = "date"
+    
+    if word:
+        sql = f"UPDATE entries SET {word} = %s WHERE date = %s"
+        try:
+            mycursor.execute(sql, (edit, date))
+            db.commit()
+            print("Update successful.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
 
 
