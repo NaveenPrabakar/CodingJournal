ca16eea0fbf25016bc43008203150bec7573b350
NaveenPrabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-07-20 12:55:10 -0500

    Update Grammer.py

--- Grammer.py
+++ Grammer.py
@@ -1,4 +1,9 @@
 from textblob import TextBlob
+import language_tool_python
+
+
+tool = language_tool_python.LanguageTool('en-US')
+
 
 #Checks for spelling errors only
 def spell(checked):
@@ -7,4 +12,13 @@ def spell(checked):
         checked[i] = str(TextBlob(checked[i]).correct())
     
     return checked
-#Need to implement the grammer checker next
+
+#Checks for grammatical errors for User entries
+def grammer(checked):
+    for i in range(len(checked)):
+        
+        correct = tool.check(checked[i])
+        final = language_tool_python.utils.correct(checked[i], correct)
+        checked[i] = final
+    
+    return checked
